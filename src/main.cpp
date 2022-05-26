2801136624c5aadbeebefe6895cc897281066db3
Ryan Yoshida <ryan.yoshida@comcast.net>
2022-05-25 21:46:23 -0700

    rtos and pin interrupt working

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #define RXD2    32 // yellow
 #define TXD2    26 // orange
 #define SLEEP   25 // grey
+#define INTPIN  36
 
 #define IridiumSerial Serial2
 
@@ -17,11 +18,48 @@
 
 // === Globals ====================================
 TFT_eSPI tft;
-// SoftwareSerial IridiumSerial;
 IridiumSBD modem = IridiumSBD(IridiumSerial, SLEEP);
+TaskHandle_t task_snd; // task_snd = message sending task, task_cmp = speed calculation task
 
 int count;
+bool is_init;
+long debounce;
 // === End Globals ================================
+
+// === Interrupts =================================
+void detect() {
+    long nxt = millis();
+    if (nxt - debounce > 40) {
+        Serial.println("Interrupt running on " + String(xPortGetCoreID()));
+        debounce = nxt;
+    }
+}
+// === End Interrupts =============================
+
+// === Tasks ======================================
+
+void send_data_task(void *param) {
+    int err;
+    Serial.println("Task running on " + String(xPortGetCoreID()));
+    Serial.println("Starting modem...");
+    err = modem.begin();
+    if (err != ISBD_SUCCESS) {
+        Serial.print("Begin failed: error");
+        Serial.println(err);
+        if (err == ISBD_NO_MODEM_DETECTED) Serial.println("No modem detected: check wiring");
+        vTaskDelete(nullptr);
+        return;
+    }
+    Serial.println("modem started");
+
+    Serial.println("Sending");
+    delay(5000);
+    Serial.println("Sent");
+    modem.sleep();
+    vTaskDelete(nullptr);
+}
+
+// === End Tasks ==================================
 
 void setup() {
 
@@ -41,10 +79,13 @@ void setup() {
 
     delay(2000);
 
+    // Initialize Pins
+    pinMode(INTPIN, INPUT);
+
+
     // = Initialize IridiumSBD
     int signalQuality = -1;
     int err;
-    delay(2000);
 
     Serial.println("Starting modem...");
     err = modem.begin();
@@ -68,32 +109,28 @@ void setup() {
         return;
     }
     Serial.print("On a scale of 0 to 5, signal quality is currently ");
-    Serial.print(signalQuality);
-//    // Send the message
-//    Serial.print("Trying to send the message.  This might take several minutes.\r\n");
-//    err = modem.sendSBDText("Hello, world!");
-//    if (err != ISBD_SUCCESS)
-//    {
-//    Serial.print("sendSBDText failed: error ");
-//    Serial.println(err);
-//    if (err == ISBD_SENDRECEIVE_TIMEOUT)
-//      Serial.println("Try again with a better view of the sky.");
-//    }
-//
-//    else
-//    {
-//    Serial.println("Hey, it worked!");
-//    }   
+    Serial.println(signalQuality);
 
-    // = Initialize Globals
+    modem.sleep();
     count = 0;
+    is_init = true;
+    debounce = 0;
 }
 
 void loop() {
+    if (is_init) { // ensure pin runs on correct core
+        attachInterrupt(digitalPinToInterrupt(INTPIN), detect, FALLING);
+        is_init = false;
+    }
+
+    Serial.println("Loop running on " + String(xPortGetCoreID()));
     tft.fillScreen(TFT_BLACK);
     tft.drawNumber(count, 120, 67);
 
-    ++count;
+    if (++count >= 30) {
+        count = 0;
+        xTaskCreate(send_data_task, "TaskSend", 10000, nullptr, 2, nullptr);
+    }
 
     delay(1000);
 }
