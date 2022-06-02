ad316718983e33d5fcd9e9722f850b45541ba27c
Ryan Yoshida <ryan.yoshida@comcast.net>
2022-06-02 03:51:19 -0700

    cleaned up repo

--- dbreset.py
+++ dbreset.py
@@ -2,7 +2,7 @@ import sqlite3
 
 con = sqlite3.connect('booty.db')
 cur = con.cursor()
-cur.execute("DROP TABLE booty")
+cur.execute("DROP TABLE IF EXISTS booty")
 cur.execute("CREATE TABLE IF NOT EXISTS booty(datetime text, latitude real, longitude real, accuracy real, data real)")
 con.close()
 
